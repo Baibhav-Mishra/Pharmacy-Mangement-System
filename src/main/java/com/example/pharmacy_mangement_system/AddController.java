a8d95276480378bd969d07af6b6381b977974292
Arnav Jagia <arnavjagia04@gmail.com>
2023-11-06 20:31:16 +0530

    back btn in add

--- src/main/java/com/example/pharmacy_mangement_system/AddController.java
+++ src/main/java/com/example/pharmacy_mangement_system/AddController.java
@@ -1,4 +1,24 @@
 package com.example.pharmacy_mangement_system;
 
-public class AddController {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AddController implements Initializable {
+    @FXML
+    void onBackButtonPress(ActionEvent event) throws IOException {
+        SceneSwitch.switchToScene(event, "Home");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
 }
