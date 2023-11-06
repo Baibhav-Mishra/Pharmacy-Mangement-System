226a4770e6ff1f005a53d8a8593a2a9e03e0e683
Arnav Jagia <arnavjagia04@gmail.com>
2023-11-06 18:20:16 +0530

    testing git

--- src/main/java/com/example/pharmacy_mangement_system/PharmacyApplication.java
+++ src/main/java/com/example/pharmacy_mangement_system/PharmacyApplication.java
@@ -16,6 +16,7 @@ public class PharmacyApplication extends Application {
         stage.setScene(scene);
         stage.show();
     }
+    // testing git
 
     public static void main(String[] args) {
         launch();
