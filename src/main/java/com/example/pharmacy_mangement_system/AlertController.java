63e60072b93ec148bb2affdce40e8d5fe52f2323
mz-hassan <muhammad1.mitmpl2022@learner.manipal.edu>
2023-11-07 01:12:37 +0530

    Basic structure of Alert Page

--- src/main/java/com/example/pharmacy_mangement_system/AlertController.java
+++ src/main/java/com/example/pharmacy_mangement_system/AlertController.java
@@ -4,11 +4,13 @@ package com.example.pharmacy_mangement_system;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
+import java.io.IOException;
+
 public class AlertController {
 
     @FXML
-    void Onlsmkcs(ActionEvent event) {
-        System.out.println("de");
+    void onBackButtonPress(ActionEvent event) throws IOException {
+        SceneSwitch.switchToScene(event, "Home");
     }
 
 }
