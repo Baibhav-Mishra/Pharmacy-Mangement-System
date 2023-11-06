c6b50b1f925a1c9bcf118e1ef3c26409b6e9ebe4
Baibhav Mishra <baibhavmishra2010@gmail.com>
2023-11-06 20:58:51 +0530

    Added alert page

---
+++ src/main/java/com/example/pharmacy_mangement_system/AlertController.java
@@ -0,0 +1,14 @@
+package com.example.pharmacy_mangement_system;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class AlertController {
+
+    @FXML
+    void Onlsmkcs(ActionEvent event) {
+        System.out.println("de");
+    }
+
+}
