00990b39807a3b9fdff9ae8afeddc7e377986858
Baibhav Mishra <baibhavmishra2010@gmail.com>
2023-11-07 16:39:00 +0530

    Updated Login.fxml

--- src/main/java/com/example/pharmacy_mangement_system/PharmacyApplication.java
+++ src/main/java/com/example/pharmacy_mangement_system/PharmacyApplication.java
@@ -12,7 +12,7 @@ public class PharmacyApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(PharmacyApplication.class.getResource("Login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 400, 500);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 500);
         stage.setTitle("Login");
         stage.setScene(scene);
         stage.show();
