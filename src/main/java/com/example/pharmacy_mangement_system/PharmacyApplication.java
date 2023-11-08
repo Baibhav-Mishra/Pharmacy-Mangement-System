2be0b82181cd5887f8f27f5a1f14aa7422c01c89
Baibhav Mishra <baibhavmishra2010@gmail.com>
2023-11-09 00:40:58 +0530

    changed size

--- src/main/java/com/example/pharmacy_mangement_system/PharmacyApplication.java
+++ src/main/java/com/example/pharmacy_mangement_system/PharmacyApplication.java
@@ -12,7 +12,7 @@ public class PharmacyApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(PharmacyApplication.class.getResource("Login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 500);
+        Scene scene = new Scene(fxmlLoader.load(), 720, 480);
         stage.setTitle("Pharmacy Management");
         stage.setScene(scene);
         stage.show();
